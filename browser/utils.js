f83ec56117b736c49ac578c9278e5e4bf2ac41dd
Nick Drane <nicholasdrane@gmail.com>
2017-06-22 17:04:39 -0500

    add post json method

--- browser/utils.js
+++ browser/utils.js
@@ -1,16 +1,17 @@
 import queryString from "query-string";
 import Promise from 'bluebird';
 
-const fetchJSON = url => {
-  return Promise.resolve(fetch(url))
-    .then(response => {
-      var contentType = response.headers.get("content-type");
-      if (contentType && contentType.indexOf("application/json") !== -1) {
-        return response.json();
-      } else {
-        throw new Error("Failed to retrieve JSON from server");
-      }
-    })
+const fetchJSON = url => Promise.resolve(fetch(url)).then(parseResponse);
+
+const postJSON = (url, body) => Promise.resolve(fetch(url, {method: 'POST', body})).then(parseResponse);
+
+const parseResponse = function (response) {
+  var contentType = response.headers.get("content-type");
+  if (contentType && contentType.indexOf("application/json") !== -1) {
+    return response.json();
+  } else {
+    throw new Error("Failed to retrieve JSON from server");
+  }
 };
 
 function definePaginationQuery(url, limit=15, offset=0) {
@@ -21,9 +22,9 @@ function definePaginationQuery(url, limit=15, offset=0) {
     }));
     offset += limit;
     return response;
-  }
+  };
 }
 
 const fetchImages = definePaginationQuery('/api/images/');
 
-export { fetchImages };
+export { fetchImages, postJSON, fetchJSON };
