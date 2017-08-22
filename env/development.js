72db315192f8e196ef11f9a0e5b56c0deb720f9f
Nick Drane <nicholasdrane@gmail.com>
2017-08-21 20:02:28 -0500

    stop tracking env

--- env/development.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  DATABASE_URI: 'postgres://localhost:5432/kelseyhagen',
-  NODE_ENV: 'development',
-  SECRET: 'NOT SUPER SECRETIVE'
-};
--- env/production.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  DATABASE_URI: process.env.POSTGRESQL_URI,
-  NODE_ENV: 'production'
-};
