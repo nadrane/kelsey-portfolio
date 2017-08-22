9a1079ab9e7abbcffb7bddff627c40474542a1a4
Nick Drane <nicholasdrane@gmail.com>
2017-08-21 20:03:38 -0500

    start mailgun

--- server/app/routes/services/email.js
+++ server/app/routes/services/email.js
@@ -1,29 +1,21 @@
-const nodemailer = require("nodemailer");
 const router = (module.exports = require("express").Router());
+const env = require("../../../../env");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.google.com",
-  port: 465,
-  secure: true, // secure:true for port 465, secure:false for port 587
-  auth: {
-    user: "username@example.com",
-    pass: "userpass"
-  }
+var mailgun = require("mailgun-js")({
+  apiKey: env.MAILGUN_API_KEY,
+  domain: env.DOMAIN
 });
 
-router.post("/", (req, res, next) => {
-  let mailOptions = {
-    from: req.body.email,
-    to: "kelsey", // TODO secrets file
-    subject: req.body.message.slice(0, 50),
+router.post("/", function(req, res, next) {
+  var data = {
+    from: req.body.name,
+    to: env.ADMIN_EMAIL,
+    subject: "A message from your website! :)",
     text: req.body.message
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      error.status = 500;
-      next(error);
-    }
-    res.sendStatus(200);
+  mailgun.messages().send(data, function(err, body) {
+    if (err) next(err);
+    console.log(body);
   });
 });
